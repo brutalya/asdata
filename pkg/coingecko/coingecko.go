98f0955062991e4a5b8a841305ddd06afd17968b
Rus <rasl2222@icloud.com>
2023-08-01 22:03:18 +0200

    More funcs with refactoring

--- pkg/coingecko/coingecko.go
+++ pkg/coingecko/coingecko.go
@@ -5,62 +5,69 @@ import (
 	"net/http"
 )
 
+func GetResponse(url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(body), nil
+}
+
+// PING
 func Ping() (string, error) {
-	resp, err := http.Get("https://api.coingecko.com/api/v3/ping")
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
+	return GetResponse("https://api.coingecko.com/api/v3/ping")
 }
 
+// GENERAL
 func GetCoinList() (string, error) {
-	resp, err := http.Get("https://api.coingecko.com/api/v3/coins/list")
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return string(body), nil
+	return GetResponse("https://api.coingecko.com/api/v3/coins/list")
 }
 
-func GetCoinMarketChart(coinId string, vsCurrency string, days string) (string, error) {
-	resp, err := http.Get("https://api.coingecko.com/api/v3/coins/" + coinId + "/market_chart?vs_currency=" + vsCurrency + "&days=" + days)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
+func GetExchangeList() (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/exchanges")
+}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
+// COINS
+func GetCoin(coinId string) (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/coins/" + coinId)
+}
 
-	return string(body), nil
+func GetCoinHistory(coinId string, date string) (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/coins/" + coinId + "/history?date=" + date)
 }
 
 func GetCoinMarket(coinId string, vsCurrency string) (string, error) {
-	resp, err := http.Get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + vsCurrency + "&ids=" + coinId + "&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
+	return GetResponse("https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + vsCurrency + "&ids=" + coinId + "&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en")
+}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
+func GetCoinMarketChart(coinId string, vsCurrency string, days string) (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/coins/" + coinId + "/market_chart?vs_currency=" + vsCurrency + "&days=" + days)
+}
 
-	return string(body), nil
+func GetCoinMarketHistory(coinId string, date string) (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/coins/" + coinId + "/market_chart?vs_currency=usd&days=" + date)
+}
+
+func GetCoinTickers(coinId string) (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/coins/" + coinId + "/tickers")
+}
+
+func GetCoinStatusUpdates(coinId string) (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/coins/" + coinId + "/status_updates")
+}
+
+// CONTRACTS
+func GetCoinContract(coinId string) (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/coins/" + coinId + "/contract")
+}
+
+func GetCoinContractMarketChart(coinId string, vsCurrency string, days string) (string, error) {
+	return GetResponse("https://api.coingecko.com/api/v3/coins/" + coinId + "/contract/" + vsCurrency + "/market_chart?vs_currency=" + vsCurrency + "&days=" + days)
 }
